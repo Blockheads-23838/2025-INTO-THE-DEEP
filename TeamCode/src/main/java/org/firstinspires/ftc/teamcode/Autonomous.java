670b754968a87efdb3148e8ba11ea779b366af4d
Leo <leoslegos1@gmail.com>
2024-09-06 17:45:32 -0700

    work with functions, motors, add blank autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -1,31 +1,3 @@
-/* Copyright (c) 2021 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 package org.firstinspires.ftc.teamcode;
 
@@ -33,7 +5,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.ArrayList;
 
 /*
  * This file contains an example of a Linear "OpMode".
@@ -63,25 +38,27 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
-public class BasicOmniOpMode_Linear extends LinearOpMode {
+@TeleOp(name="Basic: Omni Linearj,ghjh,g OpMode", group="Linear OpMode")
+public class Autonomous extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftFrontDrive = null;
-    private DcMotor leftBackDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor rightBackDrive = null;
+    private DcMotorEx leftFrontDrive = null;
+    private DcMotorEx leftBackDrive = null;
+    private DcMotorEx rightFrontDrive = null;
+    private DcMotorEx rightBackDrive = null;
+
+    ArrayList<DcMotorEx> driveMotors = new ArrayList<>();
 
     @Override
     public void runOpMode() {
 
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back");
+        leftFrontDrive  = hardwareMap.get(DcMotorEx.class, "left_front_drive");
+        leftBackDrive  = hardwareMap.get(DcMotorEx.class, "left_back_drive");
+        rightFrontDrive = hardwareMap.get(DcMotorEx.class, "right_front_drive");
+        rightBackDrive = hardwareMap.get(DcMotorEx.class, "right_back_drive");
 
         // ########################################################################################
         // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
@@ -98,6 +75,18 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
+        /*
+        Strategically
+        Taking
+        Equipment to
+        Another
+        Location
+         */
+
+        driveMotors.add(leftFrontDrive);
+        driveMotors.add(leftBackDrive);
+        driveMotors.add(rightFrontDrive);
+        driveMotors.add(rightBackDrive);
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -134,22 +123,6 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
                 rightBackPower  /= max;
             }
 
-            // This is test code:
-            //
-            // Uncomment the following code to test your motor directions.
-            // Each button should make the corresponding motor run FORWARD.
-            //   1) First get all the motors to take to correct positions on the robot
-            //      by adjusting your Robot Configuration if necessary.
-            //   2) Then make sure they run in the correct direction by modifying the
-            //      the setDirection() calls above.
-            // Once the correct motors move in the correct direction re-comment this code.
-
-            /*
-            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
-            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
-            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
-            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
-            */
 
             // Send calculated power to wheels
             leftFrontDrive.setPower(leftFrontPower);
@@ -163,4 +136,59 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
             telemetry.update();
         }
-    }}
+    }
+
+    public void goTo(double forward, double strafe, double yaw, double powercoef, boolean waitToFinish) {
+        /**
+         * Moves robot-centrically.  All is in ticks except yaw, which is approximately degrees such that 90 turns the robot very approximately 90 degrees clockwise.
+         */
+        yaw *= 11;
+        double leftFrontPower = powercoef * (forward + strafe + yaw);
+        double rightFrontPower = powercoef * (forward - strafe - yaw);
+        double leftBackPower = powercoef * (forward - strafe + yaw);
+        double rightBackPower = powercoef * (forward + strafe - yaw);
+
+        // Normalize the values so no wheel power exceeds 100%
+        // This ensures that the robot maintains the desired motion.
+        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 2000) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+            leftFrontPower *= powercoef;
+            leftBackPower *= powercoef;
+            rightFrontPower *= powercoef;
+            rightBackPower *= powercoef;
+        }
+
+        for (DcMotorEx motor : driveMotors) {
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+        leftFrontDrive.setTargetPosition((int) (forward + strafe + yaw));
+        leftBackDrive.setTargetPosition((int) (forward - strafe + yaw));
+        rightFrontDrive.setTargetPosition((int) (forward - strafe - yaw));
+        rightBackDrive.setTargetPosition((int) (forward + strafe - yaw));
+
+        for (DcMotorEx motor : driveMotors) {
+            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+
+        leftFrontDrive.setVelocity(leftFrontPower);
+        leftBackDrive.setVelocity(leftBackPower);
+        rightFrontDrive.setVelocity(rightFrontPower);
+        rightBackDrive.setVelocity(rightBackPower);
+
+        if (waitToFinish) while (leftFrontDrive.isBusy()) {
+            telemetry.addData("lf power: ", leftFrontPower);
+            telemetry.addData("lf tgt position: ", leftFrontDrive.getTargetPosition());
+            telemetry.addData("lf position: ", leftFrontDrive.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorRunDemoOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorRunDemoOpmode.java
@@ -34,6 +34,7 @@ public class MotorRunDemoOpmode extends LinearOpMode {
         // !! ----------------- CODE TO RUN AFTER PRESSING PLAY ------------------- !!
         //                         Put your actual code here.
 
+        /*
         motor.setPower(1); // Tells the motor to start going full speed forwards.
                            // The motor will continue moving after the program moves to the next instruction
                            // because this instruction only starts it, and never stops it.
@@ -41,9 +42,42 @@ public class MotorRunDemoOpmode extends LinearOpMode {
         sleep(2000); // waits 2000 milliseconds (1000 milliseconds = 1 second)
                                 // this means the motor will run full speed forwards for 2 seconds
 
-        motor.setPower(0); //  Stops sending power to the motor/stops the motor.
+         */
+/*
+        for (int i = 0; i < 10; i++) {
+            motor.setPower(i/10.0);
+            sleep(500);
+        }
+        */
+
+        /*
+        for (int i = 0; i < 21; i=i+2) {
+            motor.setPower(i/22.0);
+            sleep(1000);
+
+        */
+
+        while (true) {
+            my_function(gamepad1.left_stick_y);
+
+
+        }
+        /*
+        left_speed = gamepad1.left_stick_y - gamepad1.right_stick_x;
+        right_speed = gamepad1.left_stick_y + gamepad1.right_stick_x;
+         */
 
         // ----------- PROGRAM ENDS HERE --------------
 
+    }
+
+    private void my_function(double input) {
+        telemetry.addData("left stick y:", gamepad1.left_stick_y);
+        telemetry.update();
+        motor.setPower(input);
+    }
+
+    private double add_one(double input) {
+        return input + 1;
     }
 }
