ee4e1baeedcdb5730e3b4da02083e16a9e222898
arshansoltanzadeh <asoltanzadeh@outlook.com>
2024-10-12 21:48:27 -0700

    fixed claw - toggle with "a" button press

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class ServoTest extends LinearOpMode {
 
     private boolean open = false;
+    private boolean input = false;
     private Servo servo = null;
     private double position = Constants.claw_closed;
 
@@ -20,19 +21,24 @@ public class ServoTest extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            open = gamepad1.a;
-           if (open) {
-               position = Constants.claw_open;
-           }
-           else {
-               position = Constants.claw_closed;
-           }
+            input = gamepad1.a;
+            if (input) {
+                open = !open;
+
+
+                if (open) {
+                    position = Constants.claw_open;
+                }
+                else {
+                    position = Constants.claw_closed;
+                }
+            }
 
            servo.setPosition(position);
 
-           telemetry.addData("servo angle (degrees): ", position * 300);
-           telemetry.addData("gamepad input: ", gamepad1.a);
            telemetry.addData("servo open: ", open);
+           telemetry.addData("input (gamepad a button): ", input);
+           telemetry.addData("servo position: ", position);
            telemetry.update();
 
            sleep(20);
